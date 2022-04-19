a8fb92341c5f2722456dde954104896158db552c
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2022-04-19 19:48:46 +0200

    Avoid problems with external libraries depending on libffi.so.7 instead of 6

--- build_with_numa.sh
+++ build_with_numa.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# See https://github.com/tweag/rules_haskell/issues/481 for why we build with
+# --with-system-libffi
 ghcup install ghc 8.8.4
 ghcup compile ghc -j $(nproc) \
     -v "8.10.7" -o "8.10.7-fpic" \
     -b "8.8.4" \
-    --config $PWD/build.mk -- --disable-ld-override
+    --config $PWD/build.mk -- --disable-ld-override --with-system-libffi
